51c24ea7b1980d6c41b0f438b8758726ddeefd78
danismithmars <danismithmars@gmail.com>
2025-02-16 09:05:58 +0100

    fix: update marker image

--- src/components/MapView/index.tsx
+++ src/components/MapView/index.tsx
@@ -5,7 +5,8 @@ import { RootState, AppDispatch } from "../../store";
 import { setMapCenter } from "../../features/mapSlice";
 import styles from "./style.module.scss";
 import "leaflet/dist/leaflet.css";
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; 
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
+import L from 'leaflet';
 import 'leaflet-defaulticon-compatibility';
 
 const routeColors = ["blue", "green", "purple", "orange", "brown", "pink"];
@@ -15,6 +16,14 @@ const MapView = forwardRef<HTMLDivElement, {}>((_, ref) => {
   const mapCenter = useSelector((state: RootState) => state.map.center);
   const { stops } = useSelector((state: RootState) => state.stops);
   const { itineraries } = useSelector((state: RootState) => state.routes);
+
+
+  const markerIcon = L.icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+  });
 
   const [routeCoordinates, setRouteCoordinates] = useState<[number, number][][]>([]);
   const [stopCoordinates, setStopCoordinates] = useState<[number, number][]>([]);
@@ -50,13 +59,13 @@ const MapView = forwardRef<HTMLDivElement, {}>((_, ref) => {
         </CircleMarker>
 
         {stops.map((stop, index) => (
-          <Marker key={index} position={[stop.lat, stop.lon]}>
+          <Marker key={index} position={[stop.lat, stop.lon]} icon={markerIcon}>
             <Popup>{stop.name}</Popup>
           </Marker>
         ))}
 
         {stopCoordinates.map((stop, index) => (
-          <Marker key={`stop-${index}`} position={stop}>
+          <Marker key={`stop-${index}`} position={stop} icon={markerIcon}>
             <Popup>Stop {index + 1}</Popup>
           </Marker>
         ))}
